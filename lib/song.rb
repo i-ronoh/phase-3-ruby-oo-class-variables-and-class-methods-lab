da4eacbf35543a717776622dba05dc00ac16448e
Ivy Ronoh <ronoh.ivy@gmail.com>
2022-08-25 14:17:15 +0300

    Dne.

--- lib/song.rb
+++ lib/song.rb
@@ -33,8 +33,7 @@ class Song
       end
       artist_count
   
-      # with #tally
-      # @@artists.tally
+     
     end
   
     def self.genres
@@ -52,7 +51,6 @@ class Song
       end
       genre_count
   
-      # with #tally
-      # @@genres.tally
+     
     end
   end
